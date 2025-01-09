c8553c6667a582ea4f0f39062bc1d20eac184124
BitBucket Translations Pipeline <translations_pipeline@yarooms.com>
2025-01-09 13:06:57 +0200

    fix build conf backend 18

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,9 +2,13 @@
 
 use App\Kernel;
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+// Force production environment
+$_SERVER['APP_ENV'] = 'prod';
+$_SERVER['APP_DEBUG'] = '0';
+
+// Set other environment variables
+$_SERVER['APP_SECRET'] = '2ca64f8d83b9e89f5f19d672841d6bb8';
+$_SERVER['DATABASE_URL'] = 'postgresql://app:!ChangeMe!@127.0.0.1:5432/app?serverVersion=16&charset=utf8';
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
